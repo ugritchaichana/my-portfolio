93cb928e1d246f37876805749e7091a4c63d93d4
Booth <ugrit.booth@gmail.com>
2024-03-13 02:19:01 +0700

    update

--- src/index.js
+++ src/index.js
@@ -11,10 +11,8 @@ import { ChakraProvider } from "@chakra-ui/react";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider theme={theme}>
-    <React.StrictMode>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App/>
-    </React.StrictMode>
   </ChakraProvider>
 );
 
